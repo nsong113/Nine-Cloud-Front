90c4a646569698e6fe7124e6770a219b846aa3b5
DESKTOP-P8ALHH3\USER <dukyong99@naver.com>
2023-12-08 18:46:18 +0900

    [edit] login,signup (덕용)

--- src/components/units/login/social/LoginGoogle.tsx
+++ src/components/units/login/social/LoginGoogle.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
-
+import React from 'react';
+import { Circle } from './LoginSocial.styles';
+// Redirect URI : http://localhost:3000/auth/kakao/callback
+// JavaScript 키 : 1033e5934902e97e107547d11e0b7eaa
+// REST API 키 : 32357dbebba692f404b5417ad1630f3b
 const LoginGoogle = () => {
-  return (
-    <div>LoginGoogle</div>
-  )
-}
+  // const REST_API_KEY = process.env.REACT_APP_KAKAO_REST_API_KEY;
+  // const REDIRECT_URI = process.env.REACT_APP_KAKAO_REDIRECT_URL;
 
-export default LoginGoogle
+  const link = `https://accounts.google.com/o/oauth2/v2/auth?
+  client_id=${process.env.REACT_APP_GOOGLE_AUTH_CLIENT_ID}
+  &redirect_uri=${process.env.REACT_APP_GOOGLE_AUTH_REDIRECT_URI}
+  &response_type=code
+  &scope=email profile`;
+
+  const loginHandler = () => {
+    // window.location.href = link;
+  };
+
+  return (
+    <>
+      <Circle
+        onClick={loginHandler}
+        style={{ backgroundImage: "url('/btn_google.svg')" }}
+      />
+    </>
+  );
+};
+
+export default LoginGoogle;
---
+++ src/components/units/login/social/auth/GoogleLoginHandler.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GoogleLoginHandler = () => {
+  return (
+    <div>GoogleLoginHandler</div>
+  )
+}
+
+export default GoogleLoginHandler
