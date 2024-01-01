5b9f8e8376ba45cb964f949b2eeafaf9b312b863
DESKTOP-P8ALHH3\USER <dukyong99@naver.com>
2024-01-01 20:25:19 +0900

    [add] kakao (덕용)

--- src/components/units/community/chatting/server.js
+++ src/components/units/community/chatting/server.js
@@ -1,7 +1,8 @@
 import { io } from 'socket.io-client';
 
-const socket = io(`${process.env.REACT_APP_SERVER_URL}`, {
+const socket = io.connect(`${process.env.REACT_APP_SERVER_URL}`, {
   path: '/community/chat/socket.io',
+  cors: { origin: '*' },
 });
 
 export default socket;
