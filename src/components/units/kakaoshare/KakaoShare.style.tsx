8568e8ee30ad32e11ed697d569ebeb87fa85679a
DESKTOP-P8ALHH3\USER <dukyong99@naver.com>
2024-01-02 23:55:32 +0900

    [edit] Kakao Share (덕용)

--- src/components/units/kakaoshare/KakaoShare.style.tsx
+++ src/components/units/kakaoshare/KakaoShare.style.tsx
@@ -1,1 +1,13 @@
 import styled from 'styled-components';
+
+export const Button = styled.button`
+  background-image: url('/shareIcon.png');
+  background-color: white;
+  background-size: cover;
+  background-position: center;
+  border-radius: 10px;
+  border: none;
+  width: 35px;
+  height: 35px;
+  cursor: pointer;
+`;
